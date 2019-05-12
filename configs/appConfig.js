dde31c423f4628ce16e87bdec2f0aed506b42dc3
nmtri881994 <nmtri881994@gmail.com>
2019-05-12 21:39:33 +0700

    update server ip

--- configs/appConfig.js
+++ configs/appConfig.js
@@ -1,5 +1,5 @@
 const LOGGING_WAITING_GIF = "http://choinhanh.vn/games/thoi-trang-ladybug/images/_preloader.gif";
-const SERVER_URL = "https://35.197.133.91/server";
+const SERVER_URL = "https://10.148.0.2/server";
 
 const LANGUAGE_OPTIONS = [
     { value: 'en', label: 'English' },
