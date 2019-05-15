9ee97adfad016008a50832a21756a8dc1d1e7937
nmtri881994 <nmtri881994@gmail.com>
2019-05-15 21:44:31 +0700

    change ip to domain

--- configs/appConfig.js
+++ configs/appConfig.js
@@ -1,6 +1,6 @@
 const LOGGING_WAITING_GIF = "/public/images/_preloader.gif";
-const SERVER_URL = "http://35.197.133.91:4000/server";
-const RESOURCE_SERVER = "http://35.197.133.91:4000";
+const SERVER_URL = "http://nasal-congestion.tk//server";
+const RESOURCE_SERVER = "http://nasal-congestion.tk/4000";
 
 const LANGUAGE_OPTIONS = [
     { value: 'en', label: 'English' },
